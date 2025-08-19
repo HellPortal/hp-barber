04594d82748379c582b8d4be97e569e44a309a93
HellPortal <104918285+HellPortal@users.noreply.github.com>
2025-08-19 07:45:30 -0400

    hp-barber
    The barbershos retextured.
    
    The floors and ceiling cleaned of dirt and hair.
    
    Old chairs removed, and new chairs added.
    
    Unnecessary stuffs on the walls removed.

---
+++ hp_barber/fxmanifest.lua
@@ -0,0 +1,8 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'HellPortal'
+description 'Barbershop Retextured, Floor and Ceiling Dirt Cleaned, Old Chairs Removed, New Chairs Added, Wall Clutter Removed'
+version '1.0.0'
+
+this_is_a_map 'yes'
